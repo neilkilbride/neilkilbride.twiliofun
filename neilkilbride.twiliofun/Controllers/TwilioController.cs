a26025c416026bfb72ff30de1e281bad94c97bae
Neil Kilbride <neil@neilkilbride.com>
2017-01-02 22:49:09 +0000

    Added stripped down xml response on twilio controller.

--- neilkilbride.twiliofun/Controllers/TwilioController.cs
+++ neilkilbride.twiliofun/Controllers/TwilioController.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Net.Mime;
 using System.Runtime.Serialization;
+using System.Text;
 using System.Web.Http;
 using System.Web.Http.Results;
+using System.Web.Mvc;
 using System.Xml.Serialization;
 
 namespace neilkilbride.twiliofun.Controllers
@@ -14,13 +17,20 @@ namespace neilkilbride.twiliofun.Controllers
     {
         
         // GET api/twilio/incoming
-        [ActionName("Incoming")]
+        [System.Web.Http.ActionName("Incoming")]
         public Response GetIncoming()
         {
             return new Response() { Say = "Hello world" };
         }
 
-        [ActionName("Something")]
+        [System.Web.Http.ActionName("PlainIncoming")]
+        public HttpResponseMessage GetPlainIncoming()
+        {
+            return new HttpResponseMessage() { Content = new StringContent("<?xml version=\"1.0\" encoding=\"utf-8\"?><Response><Say>Hello world</Say></Response>", Encoding.UTF8, "application/xml" )};
+
+        }
+
+        [System.Web.Http.ActionName("Something")]
         public IEnumerable<string> GetSomething()
         {
             return new string[] { "value3", "value4" };
