0fe2ef585497cb4d5633202ef4c3b13acdadc6d9
Neil Kilbride <neil@neilkilbride.com>
2017-01-02 21:54:25 +0000

    Created Twilio API controller. Stripped out ASP.Net noise.

--- neilkilbride.twiliofun/App_Start/WebApiConfig.cs
+++ neilkilbride.twiliofun/App_Start/WebApiConfig.cs
@@ -1,9 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Threading.Tasks;
 using System.Web.Http;
+using System.Xml;
+using System.Xml.Serialization;
 using Microsoft.Owin.Security.OAuth;
+using neilkilbride.twiliofun.Controllers;
 using Newtonsoft.Json.Serialization;
 
 namespace neilkilbride.twiliofun
@@ -17,14 +24,19 @@ namespace neilkilbride.twiliofun
             config.SuppressDefaultHostAuthentication();
             config.Filters.Add(new HostAuthenticationFilter(OAuthDefaults.AuthenticationType));
 
+            //config.Formatters.XmlFormatter.UseXmlSerializer = true;
+            config.Formatters.XmlFormatter.WriterSettings.OmitXmlDeclaration = false;
+            config.Formatters.XmlFormatter.WriterSettings.Indent = true;
+
             // Web API routes
             config.MapHttpAttributeRoutes();
-
+            
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
+                routeTemplate: "api/{controller}/{action}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
         }
     }
+    
 }
---
+++ neilkilbride.twiliofun/Controllers/TwilioController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Runtime.Serialization;
+using System.Web.Http;
+using System.Web.Http.Results;
+using System.Xml.Serialization;
+
+namespace neilkilbride.twiliofun.Controllers
+{
+    public class TwilioController : ApiController
+    {
+        
+        // GET api/twilio/incoming
+        [ActionName("Incoming")]
+        public Response GetIncoming()
+        {
+            return new Response() { Say = "Hello world" };
+        }
+
+        [ActionName("Something")]
+        public IEnumerable<string> GetSomething()
+        {
+            return new string[] { "value3", "value4" };
+        }
+    }
+
+
+    [DataContract(Namespace = "")]
+    public class Response
+    {
+        [DataMember()]
+        public string Say { get; set; }
+    }
+}
